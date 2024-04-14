2a9a78562c6dd6bae42acfec8a381eb25c151a9a
james-choncholas <jim@choncholas.com>
2024-04-14 02:38:11 +0000

    Parallelize mod switch and multiplication kernels.

--- tf_shell/cc/kernels/mod_switch_kernels.cc
+++ tf_shell/cc/kernels/mod_switch_kernels.cc
@@ -53,10 +53,11 @@ class ModulusReduceContextOp : public OpKernel {
 
     // Create a new context with the reduced moduli.
     ContextVariant<T> context_var_reduced{};
-    OP_REQUIRES_OK(op_ctx, context_var_reduced.Initialize(
-                               context_var->log_n_, reduced_qs,
-                               context_var->ps_, context_var->pt_modulus_,
-                               context_var->noise_variance_, context_var->seed_));
+    OP_REQUIRES_OK(op_ctx,
+                   context_var_reduced.Initialize(
+                       context_var->log_n_, reduced_qs, context_var->ps_,
+                       context_var->pt_modulus_, context_var->noise_variance_,
+                       context_var->seed_));
 
     // Allocate a scalar output tensor and store the reduced context.
     Tensor* out;
@@ -136,22 +137,29 @@ class ModulusReduceCtOp : public OpKernel {
     auto reduced_moduli = shell_ctx->MainPrimeModuli();
     reduced_moduli.pop_back();
 
-    for (int i = 0; i < flat_output.dimension(0); ++i) {
-      // Get the ciphertext wrapper from the input.
-      SymmetricCtVariant<T> const* ct_a_var =
-          std::move(flat_a(i).get<SymmetricCtVariant<T>>());
-      OP_REQUIRES(op_ctx, ct_a_var != nullptr,
-                  InvalidArgument("SymmetricCtVariant at flat index:", i,
-                                  " did not unwrap successfully."));
+    auto ct_col_in_range = [&](int start, int end) {
+      for (int i = start; i < end; ++i) {
+        // Get the ciphertext wrapper from the input.
+        SymmetricCtVariant<T> const* ct_a_var =
+            std::move(flat_a(i).get<SymmetricCtVariant<T>>());
+        OP_REQUIRES(op_ctx, ct_a_var != nullptr,
+                    InvalidArgument("SymmetricCtVariant at flat index:", i,
+                                    " did not unwrap successfully."));
 
-      SymmetricCt result_ct =
-          ct_a_var->ct;  // Deep copy. ModReduce is in place.
-      OP_REQUIRES_OK(op_ctx, result_ct.ModReduce(t, ql_inv));
+        SymmetricCt result_ct =
+            ct_a_var->ct;  // Deep copy. ModReduce is in place.
+        OP_REQUIRES_OK(op_ctx, result_ct.ModReduce(t, ql_inv));
 
-      // Store in the output.
-      SymmetricCtVariant<T> result_var(std::move(result_ct));
-      flat_output(i) = std::move(result_var);
-    }
+        // Store in the output.
+        SymmetricCtVariant<T> result_var(std::move(result_ct));
+        flat_output(i) = std::move(result_var);
+      }
+    };
+    auto thread_pool =
+        op_ctx->device()->tensorflow_cpu_worker_threads()->workers;
+    int const cost_per_red = 618917;  // ns, measured on log_n = 11
+    thread_pool->ParallelFor(flat_output.dimension(0), cost_per_red,
+                             ct_col_in_range);
   }
 };
 
@@ -194,23 +202,30 @@ class ModulusReducePtOp : public OpKernel {
     auto main_moduli = shell_ctx->MainPrimeModuli();
     auto t = shell_ctx->PlaintextModulus();
 
-    for (int i = 0; i < flat_output.dimension(0); ++i) {
-      PolynomialVariant<T> const* pt_a_var =
-          std::move(flat_a(i).get<PolynomialVariant<T>>());
-      OP_REQUIRES(op_ctx, pt_a_var != nullptr,
-                  InvalidArgument("PolynomialVariant at flat index:", i,
-                                  " did not unwrap successfully."));
+    auto pt_col_in_range = [&](int start, int end) {
+      for (int i = start; i < end; ++i) {
+        PolynomialVariant<T> const* pt_a_var =
+            std::move(flat_a(i).get<PolynomialVariant<T>>());
+        OP_REQUIRES(op_ctx, pt_a_var != nullptr,
+                    InvalidArgument("PolynomialVariant at flat index:", i,
+                                    " did not unwrap successfully."));
 
-      // Deep copy the polynomial because ModReduce is in place.
-      OP_REQUIRES_VALUE(RnsPolynomial pt_a, op_ctx,
-                        RnsPolynomial::Create(pt_a_var->poly.Coeffs(),
-                                              pt_a_var->poly.IsNttForm()));
+        // Deep copy the polynomial because ModReduce is in place.
+        OP_REQUIRES_VALUE(RnsPolynomial pt_a, op_ctx,
+                          RnsPolynomial::Create(pt_a_var->poly.Coeffs(),
+                                                pt_a_var->poly.IsNttForm()));
 
-      OP_REQUIRES_OK(op_ctx, pt_a.ModReduceLsb(t, ql_inv, main_moduli));
+        OP_REQUIRES_OK(op_ctx, pt_a.ModReduceLsb(t, ql_inv, main_moduli));
 
-      PolynomialVariant<T> result_var(std::move(pt_a));
-      flat_output(i) = std::move(result_var);
-    }
+        PolynomialVariant<T> result_var(std::move(pt_a));
+        flat_output(i) = std::move(result_var);
+      }
+    };
+    auto thread_pool =
+        op_ctx->device()->tensorflow_cpu_worker_threads()->workers;
+    int const cost_per_red = 472447;  // ns, measured on log_n = 11
+    thread_pool->ParallelFor(flat_output.dimension(0), cost_per_red,
+                             pt_col_in_range);
   }
 };
 
